2599f142e8ecae65cdf0eb43b73874fb4b9546ec
Kristian Lalov <lalov.kr@gmail.com>
2023-08-12 15:42:42 +0300

    fix about page link

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('https://kris133753.onrender.com//about');
+  await page.goto('https://kris133753.onrender.com/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
